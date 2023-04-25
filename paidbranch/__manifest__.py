a36ca135d78b25a4bb5b1feb154389f93fd0815b
lathamagesh <canugivemeurphonenumber@gmail.com>
2023-04-25 10:29:19 +0530

    add

---
+++ paidbranch/__manifest__.py
@@ -0,0 +1,32 @@
+# code commented by jagadishmagesh1999@gmail.com
+{
+    'name': '25APRIL',
+    'version': '1.2',
+    'sequence': 1,
+    'summary': 'ODOO check',
+    'description': """ ODOO check""",
+    'category': 'Stock',
+    'author': 'no',
+    # 'price': 10.0,
+    # 'currency': 'USD',
+    'website': '',
+    'depends': [ 'web'],
+    'data': [
+        # 'wizards/inventory_movemet_view.xml',
+        # 'views/web_backend_css.xml',
+        # 'reports/report.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    # 'icons': {
+    #         '64': 'static/description/icon.png',
+    #     },
+    'images': [
+        'static/description/banner.png',
+        # 'static/description/icon.png',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
